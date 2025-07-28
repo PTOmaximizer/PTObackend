fe429666c357962b6e4588fb11c693914ed8e0b9
PTOmaximizer <ptomaximizer@gmail.com>
2025-07-27 21:07:40 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ Give a friendly summary and then recommend specific dates to take off if possibl
 
   try {
     const completion = await openai.chat.completions.create({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: prompt }],
     });
 
